4438ee3185f1778b647719e9b157ca1b5391b91e
raeima <30875444+raeima@users.noreply.github.com>
2017-08-28 08:45:03 -1000

    Add files via upload

---
+++ callingobjects.js
@@ -0,0 +1,9 @@
+var x = prompt("enter firstName, lastName, age, or grade")
+
+var students = {
+  firstName : "Raene",
+  lastName : "Imamoto",
+  age : 15,
+  grade : 11
+}
+window.alert(x.grade);
---
+++ createnewobject.js
@@ -0,0 +1,9 @@
+var newName = prompt("what is his new first name?");
+
+var cole = {
+  firstName: "Cole",
+  lastName: "House",
+  age: 15,
+  grade: 10
+}
+window.alert(cole.firstname = newName)
