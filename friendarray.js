e036edd017a121cd56181d4a9e4a3fde4feb7ec3
raeima <30875444+raeima@users.noreply.github.com>
2017-08-30 08:28:32 -1000

    Friend objects
    Created a bunch of objects and then put them in an array and then called some

---
+++ friendarray.js
@@ -0,0 +1,26 @@
+var raene = {
+	firstName: "Raene",
+  age: 15,
+  grade: 11,
+  friends: ["Alexus", "Michael", "Lauren", "Shayna"]
+};
+var michael = {
+	firstName: "Michael",
+  age: 16,
+  grade: 11,
+  friends: ["Faith", "June", "Anya", "Shayna"]
+};
+var shayna = {
+	firstName: "Shayna",
+  age: 15,
+  grade: 11,
+  friends: ["Alexus", "Michael", "Lauren", "Raene"]
+};
+var alexus = {
+	firstName: "Alexus",
+  age: 17,
+  grade: "Graduated",
+  friends: ["Alexus", "Alexus", "Raene", "Raene"]
+};
+var x = [raene, michael, shayna, alexus];
+window.alert(x[3].grade);
